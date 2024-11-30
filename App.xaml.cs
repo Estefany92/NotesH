fc2c04820a0b7b5706156c3014f806a4620c7aa1
MOCHI <MOCHI@PC>
2024-11-29 20:45:19 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesH
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
